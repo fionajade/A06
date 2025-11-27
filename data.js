0f9863cb7b3c87db882520851d9d3e9f88366094
fionajade <jadee.fiona4@gmail.com>
2025-11-27 10:44:17 +0800

    A06

---
+++ data.js
@@ -0,0 +1,66 @@
+var data = [
+{
+    picture: "assets/img/10-things.jpg"
+},
+
+{
+    picture: "assets/img/about-time.jpg"
+},
+
+{
+    picture: "assets/img/all-the-bright.jpg"
+},
+
+{
+    picture: "assets/img/dead-poets.jpg"
+},
+
+{
+    picture: "assets/img/five-feet.jpg"
+},
+
+{
+    picture: "assets/img/how-to-lose.jpg"
+},
+
+{
+    picture: "assets/img/hunger-games.jpg"
+},
+
+{
+    picture: "assets/img/idily.jpg"
+},
+
+{
+    picture: "assets/img/la-la-land.jpg"
+},
+
+{
+    picture: "assets/img/normal-people.jpg"
+},
+
+{
+    picture: "assets/img/parasite.jpg"
+},
+
+{
+    picture: "assets/img/past-lives.jpg"
+},
+
+{
+    picture: "assets/img/perks.jpg"
+},
+
+{
+    picture: "assets/img/spy-kids.jpg"
+},
+
+{
+    picture: "assets/img/uptown.jpg"
+},
+
+{
+    picture: "assets/img/we-live.jpg"
+}
+
+];
