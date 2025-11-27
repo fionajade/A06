886efba58db5036899a80eabf63d236a906986c0
fionajade <jadee.fiona4@gmail.com>
2025-11-27 22:01:24 +0800

    A06

--- data.js
+++ data.js
@@ -1,66 +1,81 @@
 var data = [
 {
-    picture: "assets/img/10-things.jpg"
+    picture: "assets/img/akari.webp",
+    name: "Akari"
 },
-
 {
-    picture: "assets/img/about-time.jpg"
+    picture: "assets/img/aoi.webp",
+    name: "Aoi"
 },
-
 {
-    picture: "assets/img/all-the-bright.jpg"
+    picture: "assets/img/gen.webp",
+    name: "Gen"
 },
-
 {
-    picture: "assets/img/dead-poets.jpg"
+    picture: "assets/img/haruichi.webp",
+    name: "Haruichi"
 },
-
 {
-    picture: "assets/img/five-feet.jpg"
+    picture: "assets/img/iharu.webp",
+    name: "Iharu"
 },
-
 {
-    picture: "assets/img/how-to-lose.jpg"
+    picture: "assets/img/isao.webp",
+    name: "Isao"
 },
-
 {
-    picture: "assets/img/hunger-games.jpg"
+    picture: "assets/img/kikoru.webp",
+    name: "Kikoru"
 },
-
 {
-    picture: "assets/img/idily.jpg"
+    picture: "assets/img/mina.webp",
+    name: "Mina"
 },
-
 {
-    picture: "assets/img/la-la-land.jpg"
+    picture: "assets/img/no8.webp",
+    name: "No. 8"
 },
-
 {
-    picture: "assets/img/normal-people.jpg"
+    picture: "assets/img/reno.webp",
+    name: "Reno"
 },
-
 {
-    picture: "assets/img/parasite.jpg"
+    picture: "assets/img/rin.webp",
+    name: "Rin"
 },
-
 {
-    picture: "assets/img/past-lives.jpg"
+    picture: "assets/img/ryo.webp",
+    name: "Ryo"
 },
-
 {
-    picture: "assets/img/perks.jpg"
+    picture: "assets/img/soshiro.webp",
+    name: "Soshiro"
 },
-
 {
-    picture: "assets/img/spy-kids.jpg"
+    picture: "assets/img/tae.webp",
+    name: "Tae"
 },
-
 {
-    picture: "assets/img/uptown.jpg"
+    picture: "assets/img/mina.webp",
+    name: "Mina"
 },
-
 {
-    picture: "assets/img/we-live.jpg"
+    picture: "assets/img/reno.webp",
+    name: "Reno"
+},
+{
+    picture: "assets/img/rin.webp",
+    name: "Rin"
+},
+{
+    picture: "assets/img/ryo.webp",
+    name: "Ryo"
+},
+{    picture: "assets/img/soshiro.webp",
+    name: "Soshiro"
+},
+{
+    picture: "assets/img/tae.webp",
+    name: "Tae"
 }
-
 ];
